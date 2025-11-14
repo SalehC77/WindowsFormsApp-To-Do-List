5563aca875e87cf667ebad0a38affd29fe4721d2
ANbadrah <nbadrah89@gmail.com>
2025-11-14 20:06:44 +0300

    Doctor: add groupbox(Doctor Infoz) , add groupbox(My session | Appoinment)

---
+++ To-Do-List/Doctor_Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class Doctor_Home : Form
+    {
+        public Doctor_Home()
+        {
+            InitializeComponent();
+        }
+    }
+}
