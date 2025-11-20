f32eaa50b621ac5a29abb4abedeb42342f16d400
ANbadrah <nbadrah89@gmail.com>
2025-11-20 13:46:03 +0300

    I entered some codes

--- To-Do-List/DoctorSession.cs
+++ To-Do-List/DoctorSession.cs
@@ -7,22 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using To_Do_List.Data;
 
 namespace To_Do_List
 {
     public partial class DoctorSession : Form
     {
+        private readonly ApplicationDbContext _db;
+        private readonly int _doctorId;
+
         public DoctorSession()
         {
             InitializeComponent();
-        }
-
-        private void dgvdoctor_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+            _doctorId = staffId;
+            _db = new ApplicationDbContext();
 
         }
 
         private void DoctorSession_Load(object sender, EventArgs e)
+        {
+            LoadDoctorSession();
+        }
+
+        private void LoadDoctorSession()
+        {
+            var data = _db.Sessions
+                .Where( s => s.StaffId == _doctorId)
+                .OrderBy( s => s.SessionDate)
+                .Select( s => new
+                {
+                    s.SessionDate,
+                    s.DurationMinutes,
+                    s.Price,
+                    s.Notes
+                }).ToList();
+
+            dgvdoctor.AutoGenerateColumns = false;
+            dgvdoctor.DataSource = data;
+        }
+
+        private void dgvdoctor_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
