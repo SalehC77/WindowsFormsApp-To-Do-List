56d5ef0a2deb416ac69199456286e5110165e21f
SalehC77 <salehbenjahlan>
2025-11-23 22:37:44 +0300

    fix Resoureces Problem

--- To-Do-List/Properties/Resources.Designer.cs
+++ To-Do-List/Properties/Resources.Designer.cs
@@ -263,11 +263,11 @@ namespace To_Do_List.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Logo {
-            get {
-                object obj = ResourceManager.GetObject("Logo", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
+        //internal static System.Drawing.Bitmap Logo {
+        //    get {
+        //        object obj = ResourceManager.GetObject("Logo", resourceCulture);
+        //        return ((System.Drawing.Bitmap)(obj));
+        //    }
+        //}
     }
 }
